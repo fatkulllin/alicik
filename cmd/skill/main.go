6ba266850b7bebf904b8adbd9ec2bc701b963cdc
Ilnur.Fatkullin <Ilnur.Fatkullin@x5.ru>
2025-05-27 16:21:49 +0500

    incr 9-10

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -2,12 +2,10 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 	"strings"
 
 	"github.com/fatkulllin/alicik/internal/logger"
-	"github.com/fatkulllin/alicik/internal/models"
 	"go.uber.org/zap"
 )
 
@@ -63,50 +61,10 @@ func run() error {
 		return err
 	}
 
+	// создаём экземпляр приложения, пока без внешней зависимости хранилища сообщений
+	appInstance := newApp(nil)
+
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
-	// оборачиваем хендлер webhook в middleware с логированием и поддержкой gzip
-	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(gzipMiddleware(webhook)))
-}
-
-func webhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	// десериализуем запрос в структуру модели
-	logger.Log.Debug("decoding request")
-	var req models.Request
-	dec := json.NewDecoder(r.Body)
-	if err := dec.Decode(&req); err != nil {
-		logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// проверяем, что пришёл запрос понятного типа
-	if req.Request.Type != models.TypeSimpleUtterance {
-		logger.Log.Debug("unsupported request type", zap.String("type", req.Request.Type))
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		return
-	}
-
-	// заполняем модель ответа
-	resp := models.Response{
-		Response: models.ResponsePayload{
-			Text: "Извините, я пока ничего не умею",
-		},
-		Version: "1.0",
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	// сериализуем ответ сервера
-	enc := json.NewEncoder(w)
-	if err := enc.Encode(resp); err != nil {
-		logger.Log.Debug("error encoding response", zap.Error(err))
-		return
-	}
-	logger.Log.Debug("sending HTTP 200 response")
+	// обернём хендлер webhook в middleware с логированием и поддержкой gzip
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(gzipMiddleware(appInstance.webhook)))
 }
